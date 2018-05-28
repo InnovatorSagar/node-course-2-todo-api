8703af747cb9cde82ef5c0708aff12298da2c93d
GyaniSagar <tyagisagar79@gmail.com>
2018-05-29 02:45:01 +0530

    Associate users with todos

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -2,14 +2,19 @@ const {ObjectID} = require('mongodb');
 
 const {Todo} = require('./../../models/todo');
 
+
+const userOneId = new ObjectId();
+const userTwoId = new ObjectId();
 const todos = [{
   _id: new ObjectID(),
-  text: 'First test todo'
+  text: 'First test todo',
+  _creator :useroneId;
 }, {
   _id: new ObjectID(),
   text: 'Second test todo',
   completed:true,
-  completedAt:333
+  completedAt:333,
+  _creator:userTwoId
 }];
 
 
